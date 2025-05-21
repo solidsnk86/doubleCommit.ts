e417b5cf98280355e3d5d5e9f6a7e26cfc7336e6
agustincalcagni <agustincalcagni@gmail.com>
2025-05-21 16:58:54 -0300

    updated uml class for python

--- Javascript/Leccion06/FuncionesCallback.js
+++ Javascript/Leccion06/FuncionesCallback.js
@@ -26,17 +26,24 @@ function miFuncionCallback() {
     console.log("Saludo asincíncrono después de 3 segundos")
 }
 
-setTimeout(miFuncionCallback, 5000)
+// setTimeout(miFuncionCallback, 5000)
 
-setTimeout( function() { console.log("Saludo asíncrono 3") }, 3000)
+// setTimeout( function() { console.log("Saludo asíncrono 3") }, 3000)
 
-setTimeout(() => {
-    console.log("Saludo asíncrono 2")
-}, 4000)
+// setTimeout(() => {
+//     console.log("Saludo asíncrono 2")
+// }, 4000)
 
-let reloj = () => {
-    let fecha = new Date()
-    console.log(`${fecha.getHours().toString().padStart(2, "0")}:${String(fecha.getMinutes()).padStart(2, "0")}:${fecha.getSeconds()}`);
-}
+//setInterval(reloj, 1000) // cada un segundo
 
-setInterval(reloj, 1000) // cada un segundo
+document.addEventListener("DOMContentLoaded", () => {
+    let reloj = () => {
+        let fecha = new Date()
+        return `${fecha.getHours().toString().padStart(2, "0")}:${String(fecha.getMinutes()).padStart(2, "0")}:${fecha.getSeconds()}`;
+    }
+
+    const timer = document.getElementById("timer")
+    setInterval(() => {
+        timer.innerHTML = reloj()
+    }, 1000)
+})
