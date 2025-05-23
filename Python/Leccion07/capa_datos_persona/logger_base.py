d93256378dd7798e4eb49250aceb275452bef845
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-05-22 22:53:43 -0300

    se agregó clase de Python (logging)

---
+++ Python/Leccion07/capa_datos_persona/logger_base.py
@@ -0,0 +1,17 @@
+import logging as log
+
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s:%(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                datefmt='%I:%M:%S %p',
+                handlers=[
+                    log.FileHandler("./capa_datos.log"),
+                    log.StreamHandler()
+                ])
+
+# Llamamos una configuración básica
+if __name__ == "__main__":
+    log.debug("log a nivel debug")
+    log.info("log a nivel info")
+    log.warning("log a nivel warning")
+    log.error("log a nivel error")
+    log.critical("log a nivel critical")
